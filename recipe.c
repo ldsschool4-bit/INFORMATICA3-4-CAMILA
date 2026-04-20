66db34243c7958c25b8b931cdb02330a7818e021
ldsschool4-bit <ldsschool4@gmail.com>
2026-04-20 19:38:13 +0000

    RECipe progress

--- recipe.c
+++ recipe.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     printf("Coconut Cranberry Dessert\n");
-
-
-
+    printf("\n");
+    printf("1 1/2 cups graham cracker crumbs\n");
+    printf("\n");
+    printf("1/2 cup butter or margarine, melted\n");
+    printf("1 can 12 ounces/340 g sweetened condensed milk\n");
+    printf("\n");
+    printf("1 cup flaked coconut\n");
+    printf("\n");
+    printf("1 cup dried cranberries\n");
+    printf("\n");
+    printf("2 cups vanilla chips\n");
+    printf("\n");
+    
+}
