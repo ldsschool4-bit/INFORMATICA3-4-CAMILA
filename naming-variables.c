bed852a7c476298492867615c424b283d5f96bfd
ldsschool4-bit <ldsschool4@gmail.com>
2026-04-21 15:05:39 +0000

    naming-Variables  progress

---
+++ naming-variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+int number; // i fixed the name of the first variable by deleting (!)
+int test_value; // I fixed the name of the second variable by spacing (_)
+int Example1; // I fixed the name of the third variable by deleting 1 and Replaced it with an uppercase letter
+float int;
+printf("Variables created successfully\n"); // I fixed the name of the second variable by deleting the dot(.)
+}
+
