c2be2f4ec0119f52b6900585a336b205ce52e13b
ldsschool4-bit <ldsschool4@gmail.com>
2026-04-21 15:48:27 +0000

    vending.c progress

--- naming-variables.c
+++ naming-variables.c
@@ -5,7 +5,7 @@ int main(void)
 int number; // i fixed the name of the first variable by deleting (!)
 int test_value; // I fixed the name of the second variable by spacing (_)
 int Example1; // I fixed the name of the third variable by deleting 1 and Replaced it with an uppercase letter
-float int;
+float int; // no spacing between words
 printf("Variables created successfully\n"); // I fixed the name of the second variable by deleting the dot(.)
 }
 
---
+++ vending.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  //Fresh orange juice
+float price1= 4.00;
+int quantity1=; 4
+char selected1= j;
+
+//Dumplings
+float price2=
+int quantity2=
+char selected2=
+}
