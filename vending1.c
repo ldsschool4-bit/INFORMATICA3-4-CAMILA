969a48e296541ef1a3cf91caebeeac72ecf229bb
ldsschool4-bit <ldsschool4@gmail.com>
2026-04-22 15:16:14 +0000

    vending machine done

---
+++ vending1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+  //Mango Gummies
+float price1= 4.00;
+int quantity1= 4;
+char selected1= 'g';
+
+  //mangoneadas
+float price2= 10.00;
+int quantity2= 22;
+char selected2= 'm';
+
+  //Spicy Ramen
+ float price3= 5.00;
+ int quantity3= 5;
+ char selected3= 'r';
+}
