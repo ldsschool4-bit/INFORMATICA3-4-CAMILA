4d5ff655205110b5a4ad987e9cc6bd38145d3f3c
ldsschool4-bit <ldsschool4@gmail.com>
2026-04-28 15:45:31 +0000

    time.c continue

---
+++ time.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("the time of movie!\n");
+
+  //Movie running time
+  running time :182 minutes
+  Start time: 5:45
+
+  //The movie will last
+  during time:3 hr and 2 min
+  End time: 8:47
+
+  int time=minutes*hours
+  int 60/182-
+}
