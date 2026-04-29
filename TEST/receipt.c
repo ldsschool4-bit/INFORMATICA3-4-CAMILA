6356c22842ff96fa248ec37d055545129995bd93
ldsschool4-bit <ldsschool4@gmail.com>
2026-04-29 14:55:31 +0000

    TEST FINISHED

---
+++ TEST/receipt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Cake
+   float price1= $20.00;
+   int quantity1=28;
+   char selected1='c';
+    //Fanta
+    float price2=$4.00
+    int quantity2=19
+    char selected2='f';
+printf("Sweet Shop\n");
+printf("---------------------------\n");
+printf("Item name: Cake\n");
+printf("item name: Fanta\n");
+printf("Total of items\n");
+int total=Cake*Fanta
+
+printf("Welcome to the Sweet Shop!\n");
+---------------------------
+Item               Price                Quantity         Code
+--------------------------------------------------------------
+//Cake  float price1= $20.00;   int quantity1=28;   'c';
+//Fanta float price2= $4.00,    int quantity2=19;   ´f´;
+}
