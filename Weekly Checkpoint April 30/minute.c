c8aac4bf283455ee459ae2fcc4a2b7e9e1e180db
ldsschool4-bit <ldsschool4@gmail.com>
2026-04-30 21:00:12 +0000

    MINUTES.C completedd.

---
+++ Weekly Checkpoint April 30/minute.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void) {
+    int total_minutes;
+    printf("Movie running time");
+    scanf("%d", &total_minutes);
+
+    int start_h;
+    int start_m;
+    printf("Start time: ");
+    scanf("%d:%d",&start_h,%start_m )
+
+    int hours = total_minutes / 60;
+    int minutes = total_minutes % 60;
+
+    int end_h= start_h + hours;
+    int end_m= start_m + min;
+
+    printf("The movie will last %d hr and %d min.\n", hours, min);
+    printf("End time: %d:%d \n", end_h, end_m);
+}
