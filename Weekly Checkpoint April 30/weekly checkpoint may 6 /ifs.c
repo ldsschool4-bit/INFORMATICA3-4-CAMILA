2a49f368684848ce5f401fe14ecebf199511cccf
ldsschool4-bit <ldsschool4@gmail.com>
2026-05-06 14:57:25 +0000

    ifs.c finished

---
+++ Weekly Checkpoint April 30/weekly checkpoint may 6 /ifs.c
@@ -0,0 +1,22 @@
+#include <studio.h>
+int main(void){
+    int a = 5; //Equal sign means assignment
+    int b = 5;
+    int c = 10;
+
+    printf("%d == %d is %d \n", a, b, a == b); //Equal sign means comparison
+    printf("%d != %d is %d \n", a, c, a != c);// !=means "not equal"
+
+    //Create a program to check if a number is negative
+    int number;
+    printf("Enter a number: ");
+    scanf(%d" , &number);
+
+        if (number < 0){
+        printf("d is a negative number.\n",number);
+    } else {
+     printf("%d is a positive number.\n",number);
+     }
+
+        }
+}
