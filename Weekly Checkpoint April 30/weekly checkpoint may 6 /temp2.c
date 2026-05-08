14e2a0b5da641f403f00f5bcb4defa9bd46abb0b
ldsschool4-bit <ldsschool4@gmail.com>
2026-05-08 20:53:32 +0000

    finished

--- Weekly Checkpoint April 30/weekly checkpoint may 6 /temp2.c
+++ Weekly Checkpoint April 30/weekly checkpoint may 6 /temp2.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 int main(void){
-    int number1, number2;
+
+    int 5, 10;
     printf("What is the Temperature!\n");
     printf("Celsius to Fahrenheit!\n");
 
     scanf("%d %d", &5, &10);
 
-    //checks if the two integers are equal.
+    //check if 5 and 10 are equal
     if(5 == 10) {
-        printf("Result: %d = %d",number1,number2);
+        printf("Result: %d = %d",5,10);
     }
 
-    //checks if number1 is greater than number2.
+    //Celsius greater than 10
     else if (5 > 10) {
         printf("Result: %d > %d", 5, 10);
     }
 
-    //checks if both test expressions are false
-    else {
-        printf("Result: %d < %d",number1, number2);
+    //Fharenheit less than 10
+    else if (5 < 10) {
+        printf("Result: %d < %d",5, 10);
     }
+
 
 }
 
