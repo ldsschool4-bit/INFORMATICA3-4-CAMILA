fa01d5066d05f09a1376702d32752eb731f489c1
ldsschool4-bit <ldsschool4@gmail.com>
2026-05-12 15:43:28 +0000

    saved

---
+++ Weekly Checkpoint April 30/calc.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+//1.Prototype
+void m_to_h(void);
+void h_to_m(void);
+int main(void){
+}
+int  = total_minutes / 60;
+    printf("Total minutes in 4 hours \n");
+
+    printf("Theres four hours \n");
+
+    printf("How many minutes in 4 hours \n");
+
+    printf("Enter your option: " );
+
+    int user response;
+    scanf("h",&user_response);
+
+     if (user response ==60){
+        //3. Call
+        m_to_h().
+    } else if(user_response==240){
+        h_to_m();
+    } else {
+        printf(")
+    }
+        }
+
+//2. Define
+void h_to_m(void){
+    float m;
+    printf("Enter the : ");
+    scanf("%h", &m);
+    float h = (h*60);
+    printf("4h = 240m \n",h,m);
+}
